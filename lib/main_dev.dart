61f8c9349de864a845bc369fc290fd37c6ae9035
Avni <avniprajapati21101@gmail.com>
2025-03-12 13:54:03 +0530

    feat: Added MainDev entry point for development

---
+++ lib/main_dev.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainDev extends StatelessWidget {
+  const MainDev({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
